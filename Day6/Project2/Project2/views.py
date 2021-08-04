c0baea2a8ebb710c641052241d472b57b9588fed
Asif <shaikhasif120@gmail.com>
2021-08-04 19:07:35 +0530

    Update

--- Day6/Project2/Project2/views.py
+++ Day6/Project2/Project2/views.py
@@ -4,7 +4,7 @@ def index(request):
     return HttpResponse('''<h1>Home Page</h1> <br> Links to other pages<br> <a href="about">About</a><br><a href="login">Login</a>''')
 
 def about(request):
-    return HttpResponse('''<h1>About Page</h1> <br> Links to other pages <br><a href="http://127.0.0.1:8000">Home</a><br><a href="login">Login</a>''')
+    return HttpResponse('''<h1>About Page</h1> <br> Links to other pages <br><a href="/">Home</a><br><a href="login">Login</a>''')
 
 def login(request):
-    return HttpResponse('''<h1>Login Page</h1> <br>Links to other pages <br><a href="http://127.0.0.1:8000">Home</a> <br> <a href="about">About</a>''')
+    return HttpResponse('''<h1>Login Page</h1> <br>Links to other pages <br><a href="/">Home</a> <br> <a href="about">About</a>''')
